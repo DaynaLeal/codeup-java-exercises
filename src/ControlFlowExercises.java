5747d95477a61c818c676c12c39d4ae95a47626c
Dayna Leal <dayna.r.leal@gmail.com>
2020-01-28 12:31:01 -0600

    working on control statements and loops exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,45 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+        //1a
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+
+
+        //1b
+//        int i = 0;
+//        do{
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//                i++;
+//            } else {
+//                i++;
+//            }
+//        } while(i <= 100);
+
+//        int i =100;
+//        do{
+//            if (i % 5 == 0){
+//                System.out.println(i);
+//                i--;
+//            } else {
+//                i--;
+//            }
+//        } while(i >= -10);
+
+        int i = 2;
+        do{
+
+        } while(i < 1000000);
+
+
+
+
+
+    }
+}
