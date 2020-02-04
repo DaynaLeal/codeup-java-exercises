720a9889bc41b19721d5a713c9082d6e3e8fec49
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-04 12:31:11 -0600

    inheritance and poly exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square extends Rectangle {
+    int side;
+
+    public Square(int side){
+        super(side, side);
+    }
+
+    public static int getArea(int side){
+        return side * ;
+    }
+
+    public static int getPerimeter(int side){
+        return (4 * side);
+    }
+}
