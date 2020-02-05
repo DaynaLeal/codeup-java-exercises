7609da483c53b1180105ba1b6d683c8e8a164707
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-05 16:57:44 -0600

    working on collections exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,31 @@
+package grades;
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    //constructor
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double gradeSize = grades.size();
+        double sum = 0;
+        for(double grade : grades){
+            sum += grade;
+        }
+        return (sum / gradeSize);
+    }
+}
