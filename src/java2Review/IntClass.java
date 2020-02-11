338882ff6c5bae55e536a490d94d060bca2b7c43
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-11 15:49:28 -0600

    beginning java2 assessment review

---
+++ src/java2Review/IntClass.java
@@ -0,0 +1,19 @@
+package java2Review;
+
+public class IntClass {
+    int value;
+
+    public int getValue(){
+        return this.value;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+
+//    public int calcSum(IntClass myInt){
+//        return
+//    }
+//    public int calcSum(IntClass valOne, IntClass valTwo){
+//        return valOne + valTwo;
+//    }
+}
